b466992b9dabf652b98d96150da6ff6b8320f7d7
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-07 21:23:09 -0400

    front end started

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ipfs links
 https://ipfs.io/ipfs/QmbMiN9qC3MeZqyv7hpRXAKTYN6YpKj9Rbcp7b9eiwK7cY/1.json
+
+navbar color #1a1a2e
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./assets/AC.png"
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav-container'>
+        <div className='logo-div'>
+          <img className='logo' src={logo} alt='logo' />
+        </div>
+        <div className='button-container'>
+          <button className='wallet-connect-btn'>Connect Wallet</button>
+        </div>
+      </div>
+      <div>
+        <div className='header-container'>
+          <h1 className='header'>Mint Your AI Alien Today!!</h1>
+          <h4>Free Mint</h4>
+        </div>
+        <div>
+            <h2>Recent Mints!</h2>
+        </div>
+      </div>
+     
+     
+      <footer>Built By Crypted Sante</footer>
+      
     </div>
   );
 }
