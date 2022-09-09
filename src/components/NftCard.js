ed0d2e3c51f51ea5ff50f41fc87ee096e19739e5
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-08 20:02:33 -0400

    added alt

--- src/components/NftCard.js
+++ src/components/NftCard.js
@@ -9,21 +9,21 @@ function NftCards() {
     <div className='nft-container-grid'>
         <div className='nft-card-container'>
             <div className='nft-img-container'>
-                <img className='nft-img' src={img3} />
+                <img className='nft-img' src={img3} alt="nft image1" />
                 <h2 className='gradient-text'>Alien 1</h2>
             </div>
         </div>
         
         <div className='nft-card-container'>
             <div className='nft-img-container'>
-                <img className='nft-img' src={img17} />
+                <img className='nft-img' src={img17} alt="nft image2" />
                 <h2 className='gradient-text'>Alien 1</h2>
             </div>
         </div>
         
         <div className='nft-card-container'>
             <div className='nft-img-container'>
-                <img className='nft-img' src={img23} />
+                <img className='nft-img' src={img23} alt="nft image3" />
                 <h2 className='gradient-text'>Alien 1</h2>
             </div>
         </div>
